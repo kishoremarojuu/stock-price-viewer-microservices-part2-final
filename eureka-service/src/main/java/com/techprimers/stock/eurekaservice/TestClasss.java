6bdaaa90b2a9e05aa0dabe3a0999156843c48b0a
kishoremarojuu' ' <kishoremarojuu>
2018-06-16 16:20:24 -0500

    initial commit, adding test file from local eclipse to reflect the changes in github

---
+++ eureka-service/src/main/java/com/techprimers/stock/eurekaservice/TestClasss.java
@@ -0,0 +1,12 @@
+
+package com.techprimers.stock.eurekaservice;
+
+public class TestClasss {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("adding in local eclipse to reflect the changes in github through source tree");
+		
+	}
+}
